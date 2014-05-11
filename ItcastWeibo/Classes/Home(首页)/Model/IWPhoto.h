ca3ed65aebf3f3a567d3d410872ba52e9c4f7a35
Stone <1307374057@qq.com>
2014-05-11 12:08:12 +0800

    配图

---
+++ ItcastWeibo/Classes/Home(首页)/Model/IWPhoto.h
@@ -0,0 +1,16 @@
+//
+//  IWPhoto.h
+//  ItcastWeibo
+//
+//  Created by apple on 14-5-11.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IWPhoto : NSObject
+/**
+ *  缩略图
+ */
+@property (nonatomic, copy) NSString *thumbnail_pic;
+@end
---
+++ ItcastWeibo/Classes/Home(首页)/Model/IWPhoto.m
@@ -0,0 +1,13 @@
+//
+//  IWPhoto.m
+//  ItcastWeibo
+//
+//  Created by apple on 14-5-11.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import "IWPhoto.h"
+
+@implementation IWPhoto
+
+@end
